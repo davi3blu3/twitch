e5479a5b7ad35d1d58c049ce8bbe52ba23adaa4d
Dave Harned <dharned@gmail.com>
2016-04-03 11:44:39 -0500

    tabs select displayed content

--- js/call.js
+++ js/call.js
@@ -1,3 +1,28 @@
 // streaming channels
 
 var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "brunofin", "comster404", "sabercrit", "vinesauce", "exinthevatican"];
+
+
+
+// control displayed channels via selected tab
+
+$("#all-chan-tab").on('click', function() {
+	$('.nav-tabs > li').removeClass('active');
+	$(this).addClass('active');
+	$('.online').show();
+	$('.offline').show();
+});
+
+$("#on-chan-tab").on('click', function() {
+	$('.nav-tabs > li').removeClass('active');
+	$(this).addClass('active');
+	$('.online').show();
+	$('.offline').hide();
+});
+
+$("#off-chan-tab").on('click', function() {
+	$('.nav-tabs > li').removeClass('active');
+	$(this).addClass('active');
+	$('.online').hide();
+	$('.offline').show();
+});
