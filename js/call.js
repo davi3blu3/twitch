68b35e3bcd308b53a6a2cce57b2077bcbd547819
Dave Harned <dharned@gmail.com>
2016-04-05 23:29:08 -0500

    online channels sorted to top

--- js/call.js
+++ js/call.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
             } else if (data.stream === undefined) {
                 $(".container").append('<div class="row offline"><div class="col-sm-1"></div><div class="col-sm-2"><h4 class="channel-name">' + channel + '</h4></div><div class="col-sm-9"><h4 class="channel-status">Not Found</h4></div></div>');
             } else {
-                $(".container").append('<div class="row online"><div class="img-col col-sm-1"><img class="channel-logo" src="' + data.stream.channel.logo + '" alt="freecodecamp logo"/></div><div class="col-sm-2"><h4 class="channel-name">' + data.stream.channel.display_name + '</h4></div><div class="col-sm-9"><h4 class="channel-status">' + data.stream.channel.status + '</h4></div></div>');
+                $(".nav").after('<div class="row online"><div class="img-col col-sm-1"><img class="channel-logo" src="' + data.stream.channel.logo + '" alt="freecodecamp logo"/></div><div class="col-sm-2"><h4 class="channel-name">' + data.stream.channel.display_name + '</h4></div><div class="col-sm-9"><h4 class="channel-status">' + data.stream.channel.status + '</h4></div></div>');
             }
         });
     });
